67fd9bcb1aa0e2628f7e8c9ccfa104394e77fe84
Kauã Pereira <kaua.pereira102004@gmail.com>
2024-11-01 15:32:56 -0300

    biuld: integration of measurement images and creation of measurement inputs

--- pages/home.py
+++ pages/home.py
@@ -28,20 +28,60 @@ def render():
 
     with tab1: 
         
+         
         st.write("Informe suas Metricas:")
-        sexo = st.radio(
+        sex = st.radio(
         "Qual seu sexo?",
         ("Homem", "Mulher")
         )
         
-        col4 , col5, col6 = st.columns(3)
-        with col4:
-            Altura = st.number_input("Digite sua Altura (CM):",min_value=1)
-        with col5:
-            Peso = st.number_input("Digite seu Peso (KG):",min_value=1.00)
-        with col6:
-            Idade = st.number_input("Digite sua idade:", min_value=0, format="%d")
-    
+        col1, col2 = st.columns([0.2, 1])  # Proporção para a imagem e inputs
+
+        with col1:
+            if sex == "Homem":
+                st.image("./assets/imgs/Men_Measurements.png", use_column_width=True) 
+            else:
+                st.image("./assets/imgs/Woman_Measurements.png", use_column_width=True)
+
+        with col2:
+            col2_1 = st.columns(3)  
+
+            with col2_1[0]:
+                neck = st.number_input("Circunferência do Pescoço (CM):", min_value=1, key="neck")
+
+            with col2_1[1]:
+                chest = st.number_input("Circunferência do Peito (CM):", min_value=1, key="chest")
+
+            with col2_1[2]:
+                waist = st.number_input("Circunferência da Cintura (CM):", min_value=1, key="waist")
+
+            col2_2 = st.columns(3) 
+
+            with col2_2[0]:
+                hip = st.number_input("Circunferência do Quadril (CM):", min_value=1, key="hip")
+
+            with col2_2[1]:
+                thigh = st.number_input("Circunferência da Coxa (CM):", min_value=1, key="thigh")
+
+            with col2_2[2]:
+                calf = st.number_input("ircunferência da Panturrilha (CM):", min_value=1, key="calf")
+            
+            col2_3 = st.columns(4)
+
+            with col2_3[1]:
+                aa = st.number_input("Circunferência do Quadril (CM):", min_value=1, key="aa")
+
+            with col2_3[2]:
+                thaigh = st.number_input("Circunferência da Coxa (CM):", min_value=1, key="thaigh")
+        
+        col3 = st.columns(3)
+        with col3[0]:
+            height = st.number_input("Digite sua Altura (CM):",min_value=1)
+        with col3[1]:
+            weight = st.number_input("Digite seu Peso (KG):",min_value=1.00)
+        with col3[2]:
+            age = st.number_input("Digite sua idade:", min_value=0, format="%d")
+        
         if st.button("Calcular"):
             st.session_state.show_more = True
 
@@ -49,8 +89,8 @@ def render():
             
             row = st.columns(3)
             tile = row[0].container(border=True)
-            AlturaIMC = Altura/ 100
-            IMC = Peso / (AlturaIMC ** 2)
+            heightIMC = height/ 100
+            IMC = weight / (heightIMC ** 2)
             tile.write(f"<p style='text-align: center;'>IMC (Índice de Massa Corporal)</br>{IMC:.2f}</p>", unsafe_allow_html=True)
             row[0].write("##### Explicação:")
 
@@ -75,8 +115,8 @@ def render():
                 with col2: st.write("A obesidade pode representar perigos como problemas cardiacos, diabetes tipo 2, hipertensão e problemas ortopedicos, por conta disso Procure um Medico!")
 
             tile = row[1].container(border=True)
-            if sexo == "Homem": TMB = 88.36 + (13.4 * Peso) + (4.8 * Altura) - (5.7 * Idade)
-            elif sexo == "Mulher": TMB = 447.6 + (9.2 * Peso) + (3.1 * Altura) - (4.3 * Idade)
+            if sex == "Homem": TMB = 88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)
+            elif sex == "Mulher": TMB = 447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)
             tile.write(f"<p style='text-align: center;'>Taxa Metabólica Basal (TMB)</br>{TMB:.2f} Kcal</p>", unsafe_allow_html=True)
             row[1].write("##### Gasto Calórico Diário Total (GCDT):")
             cols = row[1].columns([1, 5, 1]) 
@@ -87,7 +127,7 @@ def render():
             with cols[1]: st.write(f"Muito intenso (2x/dia ou trabalho físico pesado): {TMB * 1.9:.2f} kcal")
 
             tile = row[2].container(border=True)
-            watter_per_kg = (Peso * 35) / 1000
+            watter_per_kg = (weight * 35) / 1000
             tile.write(f"<p style='text-align: center;'>Ingestão de Água por KG</br>{watter_per_kg} Litros</p>", unsafe_allow_html=True)
             row[2].write("##### Quantidade:")
             cols = row[2].columns([1, 5, 1]) 
